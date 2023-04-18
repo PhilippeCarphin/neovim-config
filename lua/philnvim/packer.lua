4bb4163a13315838e0502d5eb57008410708d5e6
Philippe Carphin <philippe.carphin@polymtl.ca>
2023-04-18 10:04:11 -0400

    Use package vim-lastplace to open files at last location

--- lua/philnvim/packer.lua
+++ lua/philnvim/packer.lua
@@ -53,5 +53,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+    use("farmergreg/vim-lastplace")
 end)
 
