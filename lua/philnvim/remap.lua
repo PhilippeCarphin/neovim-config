834d0dcc24d7b7038ce3c509f936fed7af5af7fe
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-04-19 17:57:28 -0400

    fixup esc

--- lua/philnvim/remap.lua
+++ lua/philnvim/remap.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>nv", ":Telescope find_files cwd=~/.config/nvim/<CR>")
-vim.keymap.set("i", "jk", "<ESC>l")
+vim.keymap.set("i", "jk", "<C-O>:stopinsert<CR>")
 -- Prevent cursor from moving back after pressing <ESC> to exit insert mode
 -- Can't believe that I've lived without this for so long!
 -- Also added to the jk binding.
-vim.keymap.set("i", "<ESC>", "<ESC>l")
+vim.keymap.set("i", "<ESC>", "<C-O>:stopinsert<CR>")
 -- Same idea, I also press "l" 99.9% of the time after having pasted something
 vim.keymap.set("n", "p", "pl")
 vim.keymap.set("n", "P", "Pl")
