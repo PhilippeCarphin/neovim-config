1586f717d671b7a7658f3714bdd2251e0d2cbee3
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-05-05 19:39:27 -0400

    Added stuff

--- lua/philnvim/packer.lua
+++ lua/philnvim/packer.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
 
 	use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
 	use("nvim-treesitter/playground")
+    use("tomasr/molokai")
 
 	use("theprimeagen/harpoon")
 	use("mbbill/undotree")
@@ -62,5 +63,8 @@ return require('packer').startup(function(use)
             org.setup{}
         end
     }
+    use('eandrju/cellular-automaton.nvim')
+    use('ntpeters/vim-better-whitespace')
+    use('ryanpcmcquen/true-monochrome_vim')
 end)
 
--- lua/philnvim/remap.lua
+++ lua/philnvim/remap.lua
@@ -9,3 +9,5 @@ vim.keymap.set("i", "<ESC>", "<C-O>:stopinsert<CR>")
 -- Same idea, I also press "l" 99.9% of the time after having pasted something
 vim.keymap.set("n", "p", "pl")
 vim.keymap.set("n", "P", "Pl")
+vim.keymap.set("n", "<leader>fml", ":CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>fmg", ":CellularAutomaton game_of_life<CR>")
